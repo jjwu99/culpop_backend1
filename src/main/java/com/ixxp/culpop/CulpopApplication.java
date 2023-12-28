540e2df182516bdfe26a9d7f283853d4b8914f56
juyeon <99jjwu@gmail.com>
2023-12-28 14:16:51 +0900

    frame

---
+++ src/main/java/com/ixxp/culpop/CulpopApplication.java
@@ -0,0 +1,13 @@
+package com.ixxp.culpop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CulpopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CulpopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ixxp/culpop/CulpopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ixxp.culpop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CulpopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
